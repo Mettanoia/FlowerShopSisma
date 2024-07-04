4c273aa507864928c655223044b86a36fd8748f7
miguel <miguelsunnata@gmail.com>
2024-07-04 12:19:21 +0200

    addDecoration and deleteDecoration implemented.

--- src/main/java/com/itacademy/sigma_team/cli_controller/CliController.java
+++ src/main/java/com/itacademy/sigma_team/cli_controller/CliController.java
@@ -2,6 +2,7 @@ package com.itacademy.sigma_team.cli_controller;
 
 import com.itacademy.sigma_team.decorations.use_cases.AddDecorationUseCase;
 import com.itacademy.sigma_team.decorations.use_cases.DeleteDecorationUseCase;
+import com.itacademy.sigma_team.domain.Decoration;
 import com.itacademy.sigma_team.domain.Flower;
 import com.itacademy.sigma_team.flowers.use_cases.AddFlowerUseCase;
 import com.itacademy.sigma_team.flowers.use_cases.DeleteFlowerUseCase;
@@ -27,11 +28,11 @@ public final class CliController {
     }
 
     private void addTree() {}
-    private void addDecoration() {}
+    private void addDecoration(Decoration decoration) {this.addDecorationUseCase.exec(decoration);}
     private void addTicket() {}
     private void deleteFlower(Flower flower) {this.deleteFlowerUseCase.exec(flower);}
     private void deleteTree() {}
-    private void deleteDecoration() {}
+    private void deleteDecoration(Decoration decoration) {this.deleteDecorationUseCase.exec(decoration);}
     private void deleteTicket() {}
     private void printStock() {}
     private void printPurchaseHistory() {}
