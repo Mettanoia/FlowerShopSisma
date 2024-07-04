eba46e42d63d673d6ec669ddb579b8d0b9b00d83
miguel <miguelsunnata@gmail.com>
2024-07-04 12:15:50 +0200

    DeleteDecorationUseCase and AddDecorationUseCase added to CliController constructor.

--- src/main/java/com/itacademy/sigma_team/cli_controller/CliController.java
+++ src/main/java/com/itacademy/sigma_team/cli_controller/CliController.java
@@ -1,5 +1,7 @@
 package com.itacademy.sigma_team.cli_controller;
 
+import com.itacademy.sigma_team.decorations.use_cases.AddDecorationUseCase;
+import com.itacademy.sigma_team.decorations.use_cases.DeleteDecorationUseCase;
 import com.itacademy.sigma_team.domain.Flower;
 import com.itacademy.sigma_team.flowers.use_cases.AddFlowerUseCase;
 import com.itacademy.sigma_team.flowers.use_cases.DeleteFlowerUseCase;
@@ -8,10 +10,14 @@ public final class CliController {
 
     private final AddFlowerUseCase addFlowerUseCase;
     private final DeleteFlowerUseCase deleteFlowerUseCase;
+    private final AddDecorationUseCase addDecorationUseCase;
+    private final DeleteDecorationUseCase deleteDecorationUseCase;
 
-    public CliController(AddFlowerUseCase addFlowerUseCase, DeleteFlowerUseCase deleteFlowerUseCase) {
+    public CliController(AddFlowerUseCase addFlowerUseCase, DeleteFlowerUseCase deleteFlowerUseCase, AddDecorationUseCase addDecorationUseCase, DeleteDecorationUseCase deleteDecorationUseCase) {
         this.addFlowerUseCase = addFlowerUseCase;
         this.deleteFlowerUseCase = deleteFlowerUseCase;
+        this.addDecorationUseCase = addDecorationUseCase;
+        this.deleteDecorationUseCase = deleteDecorationUseCase;
     }
 
     private void createFlowerShop(){}
