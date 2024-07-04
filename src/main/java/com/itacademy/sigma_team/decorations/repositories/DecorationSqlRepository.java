7d52404798e2048a559fecc679db5b3d46123766
miguel <miguelsunnata@gmail.com>
2024-07-04 12:13:02 +0200

    Repositories skeletons added.

---
+++ src/main/java/com/itacademy/sigma_team/decorations/repositories/DecorationSqlRepository.java
@@ -0,0 +1,20 @@
+package com.itacademy.sigma_team.decorations.repositories;
+
+import com.itacademy.sigma_team.gateways.DecorationGateway;
+
+public final class DecorationSqlRepository implements DecorationGateway {
+    @Override
+    public DecorationDTO getDecoration(Long decorationId) {
+        return null;
+    }
+
+    @Override
+    public void deleteDecoration(DecorationDTO decorationDTO) {
+
+    }
+
+    @Override
+    public void addDecoration(DecorationDTO decorationDTO) {
+
+    }
+}
