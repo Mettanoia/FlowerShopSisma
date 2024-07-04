b85a23faa3ad7685674cbc12a6c300b946dbdaef
miguel <miguelsunnata@gmail.com>
2024-07-04 10:28:47 +0200

    Repositories skeletons added.

---
+++ src/main/java/com/itacademy/sigma_team/flowers/repositories/FlowerSqlRepository.java
@@ -0,0 +1,21 @@
+package com.itacademy.sigma_team.flowers.repositories;
+
+import com.itacademy.sigma_team.flowers.use_cases.FlowerDTO;
+import com.itacademy.sigma_team.flowers.use_cases.FlowerGateway;
+
+public final class FlowerSqlRepository implements FlowerGateway {
+    @Override
+    public void addFlower(FlowerDTO flowerDTO) {
+
+    }
+
+    @Override
+    public FlowerDTO getFlower(Long flowerId) {
+        return null;
+    }
+
+    @Override
+    public void deleteFlower(FlowerDTO flowerDTO) {
+
+    }
+}
