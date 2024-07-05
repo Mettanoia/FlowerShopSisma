8fef7445a8d7ccb21d81fce661a6c2a2693607ca
Sonia Vera <douxmort@gmail.com>
2024-07-05 16:54:06 +0200

    TreeGateway

---
+++ src/main/java/com/itacademy/sigma_team/gateways/TreeGateway.java
@@ -0,0 +1,11 @@
+package com.itacademy.sigma_team.gateways;
+
+
+import com.itacademy.sigma_team.trees.repositories.TreeDTO;
+
+public interface TreeGateway {
+    void addTree(TreeDTO treeDTO);
+    TreeDTO getTree(Long treeId);
+    void deleteTree(TreeDTO treeDTO);
+}
+
