5733110636abb940e8e651bfe0b887deaa378d4b
SVG <54545515+Dxmrt@users.noreply.github.com>
2024-07-05 17:01:43 +0200

    Delete src/main/java/com/itacademy/sigma_team/trees/use_cases/TreeGateway.java
    Moved and added TreeGateway to gateways

--- src/main/java/com/itacademy/sigma_team/trees/use_cases/TreeGateway.java
+++
@@ -1,7 +0,0 @@
-package com.itacademy.sigma_team.trees.use_cases;
-
-import com.itacademy.sigma_team.trees.repositories.TreeDTO;
-
-public interface TreeGateway {
-    void addTree(TreeDTO treeDTO);
-}
