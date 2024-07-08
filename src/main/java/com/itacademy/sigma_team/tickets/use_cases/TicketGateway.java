0fc1834b3f0a77b5d34c58e29de71d4786536058
miguel <miguelsunnata@gmail.com>
2024-07-08 09:35:11 +0200

    Repositories skeletons added. TicketGatewat relocated.

--- src/main/java/com/itacademy/sigma_team/tickets/use_cases/TicketGateway.java
+++ src/main/java/com/itacademy/sigma_team/tickets/use_cases/TicketGateway.java
@@ -1,7 +1,7 @@
-package com.itacademy.sigma_team.gateways;
+package com.itacademy.sigma_team.tickets.use_cases;
 
 import com.itacademy.sigma_team.gateways.readers.Adder;
 import com.itacademy.sigma_team.gateways.readers.Deleter;
 import com.itacademy.sigma_team.tickets.repositories.TicketDTO;
 
-interface TicketGateway extends Adder<TicketDTO>, Deleter<TicketDTO> { }
+public interface TicketGateway extends Adder<TicketDTO>, Deleter<TicketDTO> { }
