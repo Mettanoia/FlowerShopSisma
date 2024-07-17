c4c844c34bcf7d33ce4f041fe08136fa752271b5
miguel <miguelsunnata@gmail.com>
2024-07-17 21:37:12 +0200

    Clear console only debian now

--- src/main/java/com/itacademy/sigma_team/cli_controller/CliController.java
+++ src/main/java/com/itacademy/sigma_team/cli_controller/CliController.java
@@ -190,21 +190,15 @@ public final class CliController {
         }
     }
 
-    // Method to clear the console
     public void clearConsole() {
         try {
-            if (System.getProperty("os.name").contains("Windows")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else {
-                System.out.print("\033[H\033[2J");
-                System.out.flush();
-            }
+            // Clear console using ANSI escape codes
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
         } catch (Exception e) {
             System.out.println("Error clearing console: " + e.getMessage());
         }
     }
-
-
 
     private void createTicketMenu() {
 
