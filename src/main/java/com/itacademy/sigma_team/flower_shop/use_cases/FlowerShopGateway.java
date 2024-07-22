833e590664b6fb3c572684921f3b038986220ac8
miguel <miguelsunnata@gmail.com>
2024-07-22 09:49:50 +0200

    Fork

---
+++ src/main/java/com/itacademy/sigma_team/flower_shop/use_cases/FlowerShopGateway.java
@@ -0,0 +1,7 @@
+package com.itacademy.sigma_team.flower_shop.use_cases;
+
+import com.itacademy.sigma_team.flower_shop.repositories.FlowerShopDTO;
+import com.itacademy.sigma_team.gateway_mixins.writers.Adder;
+
+public interface FlowerShopGateway extends Adder<FlowerShopDTO> {
+}
